962f41bf0579921495be18c9592b6e0dd9b749a4
zhigu34 <zhi.gu@foxmail.com>
2026-09-20 13:47:19 +0800

    backend: add ZLMediaKit integration package

---
+++ backend/app/integrations/zlm/__init__.py
@@ -0,0 +1,13 @@
+from .adapter import (
+    ZlmAdapter,
+    ZlmIntegrationError,
+    ZlmMediaProbe,
+    ZlmTrackProbe,
+)
+
+__all__ = [
+    "ZlmAdapter",
+    "ZlmIntegrationError",
+    "ZlmMediaProbe",
+    "ZlmTrackProbe",
+]
