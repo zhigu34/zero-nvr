78169f7002a0ad9349205f8ca5c65c60c214f5d1
zhigu34 <zhi.gu@foxmail.com>
2026-09-20 14:01:34 +0800

    backend: add ONVIF integration package

---
+++ backend/app/integrations/onvif/__init__.py
@@ -0,0 +1,17 @@
+from .adapter import (
+    OnvifAdapter,
+    OnvifDeviceInfo,
+    OnvifDiscoveryCandidate,
+    OnvifInspection,
+    OnvifIntegrationError,
+    OnvifProfileProbe,
+)
+
+__all__ = [
+    "OnvifAdapter",
+    "OnvifDeviceInfo",
+    "OnvifDiscoveryCandidate",
+    "OnvifInspection",
+    "OnvifIntegrationError",
+    "OnvifProfileProbe",
+]
