1ce1959eaf445f8eb9252930315cfc751e54758a
zhigu34 <zhi.gu@foxmail.com>
2026-09-20 18:29:53 +0800

    backend: add restic integration package

---
+++ backend/app/integrations/restic/__init__.py
@@ -0,0 +1,11 @@
+from .adapter import (
+    ResticAdapter,
+    ResticBackupResult,
+    ResticIntegrationError,
+)
+
+__all__ = [
+    "ResticAdapter",
+    "ResticBackupResult",
+    "ResticIntegrationError",
+]
